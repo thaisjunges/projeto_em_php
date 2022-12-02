a3f5f1978e57bb74b7e9888eeb2bae100262903b
Thais.Junges <ferreira.thais98@hotmail.com>
2022-12-02 17:38:09 -0300

    Curso PHP

--- ifelse_praticando_mais_um_pouco.php
+++ ifelse_praticando_mais_um_pouco.php
@@ -7,6 +7,25 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
     </head>
+    <style>
+/*
+body{
+    font-family: 'Nerko One', cursive;
+}
+
+p{
+    font-size: 28px;
+}
+
+div{
+    
+}
+
+.texto{
+    border-radius: 2px solid red;
+}
+*/
+    </style>
     <body>
 
         <?php 
@@ -23,27 +42,24 @@
         } 
         ?>
 
-        <div>
-            <div>
-                <h1>Detalhes do Pedido</h1>
-            </div>
-             
-        <p>Possui cartão da loja?
-            <?php 
+            <div id="container">
+                <div id="box" >
+                    
+                  <img src="img/detalhes.png" alt="">  <h1 id="texto">Detalhes do Pedido</h1>
+
+                    <p>Possui cartão da loja?
+                
+                <?php 
             if($usuario_possui_cartao_loja) {
                 echo 'SIM';
             } else {
                 echo 'NÃO';
             }
-            ?>
+            ?> 
         </p>
-
-        <p>Valor da compra: <?= $valor_compra ?></p>
-        </div>
-       
-            
         
-            <div>
+        <p>Valor da compra: <?= $valor_compra ?></p>
+       
                  <p>Recebeu desconto no frete?
             <?php
             if($recebeu_desconto_frete) {
@@ -55,7 +71,12 @@
         </p>
 
         <p>Valor da Frete: <?= $valor_frete ?></p>
-    </body>
             </div>
+                </div>
+                
+                
+ 
+    </body>
+            
        
 </html>
